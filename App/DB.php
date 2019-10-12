63f8455ea7d427cf40dfa78351f0efd4f4dbbd6f
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-10-12 17:31:38 +0200

    Setup dotenv to store sensitive data like passwords

--- App/DB.php
+++ App/DB.php
@@ -3,6 +3,9 @@
 namespace App;
 
 class DB {
+
+    public $db;
+
     public static function init() {
         
     }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-require "";
+require "./../vendor/autoload.php";
+
+//====================================================
+//.env contains sensitive data
+//.env.example contains what settings need to be there
+//====================================================
+
+$dotenv = Dotenv\Dotenv::create(__DIR__.'\..');
+$dotenv->load();
+
+echo "NICE".getenv('TESTSETTING'); //NICECOOL
