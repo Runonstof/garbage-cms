064344c633da7b0c49b80fb99d8ea682cfd2cd9e
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-10-20 20:19:23 +0200

    Added full bootstrap instead of only its grid

--- App/helpers.php
+++ App/helpers.php
@@ -50,17 +50,16 @@ if(!function_exists('route')) {
 
 if(!function_exists('mix')) {
     function mix($file) {
-        return url().'public/'.$file.'?v='.uniqid();
+        return url().'/public/'.$file.'?v='.uniqid();
     }
 }
 
 if(!function_exists('url')) {
     function url(){
         return sprintf(
-          "%s://%s%s",
+          "%s://%s",
           isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-          $_SERVER['SERVER_NAME'],
-          $_SERVER['REQUEST_URI']
+          $_SERVER['SERVER_NAME']
         );
       }
       
