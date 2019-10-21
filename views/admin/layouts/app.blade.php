c3789a008bf369142973aab9f24f8b56ef29d288
jianweilee128 <47060167+jianweilee128@users.noreply.github.com>
2019-10-21 19:45:38 +0800

    updated app.blade.php

--- views/admin/layouts/app.blade.php
+++ views/admin/layouts/app.blade.php
@@ -1,10 +1,10 @@
 <html>
     <head>
         <title>Garbage CMS</title>
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/admin/app.css') }}">
     </head>
     <body>
         @yield('content')
-        
     </body>
+    <script type="text/javascript" src="{{ mix('js/admin/app.js') }}"></script>
 </html>
