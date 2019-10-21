238c8d982b099d455d92d7ac44089f45f02ded7a
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-10-22 01:54:09 +0200

    Custom forn thingy

--- resources/js/forms/form.js
+++ resources/js/forms/form.js
@@ -1,3 +1,6 @@
+const $ = require('jquery');
+
+
 $(function(){
     var isSubmitting = false;
     $('form[data-garbage-cms-form]').on('submit', function(ev){
@@ -5,12 +8,34 @@ $(function(){
         if(!isSubmitting) {
             isSubmitting = true;
 
-            Axios.post($(this).attr('action')).then(function(response){
-                if(response.data.redirect) {
-                    window.location = response.data.redirect;
+            var formData = {};
+            var $inputs = $(this).find('[name][type!=\'submit\']');
+            $inputs.each(function(input){
+                formData[$(this).attr('name')] = $(this).val()||$(this).html();
+            });
+
+            Axios[$(this).attr('method').toLowerCase()]($(this).attr('action'), $.param(formData)).then(function(res){
+                if(res.data.redirect) {
+                    window.location = res.data.redirect;
                 }
-                if(response.data.message) {
-                    alert( response.data.message );
+                if(res.data.message) {
+                    $.notify({
+                        title: res.data.title||'',
+                        message: res.data.message,
+                    },
+                    {
+                        placement: {
+                            from: "top",
+                            align: "right"
+                        },
+                        type: res.data.type||'danger',
+                        z_index: 1031,
+                        delay: res.data.delay||2500,
+                        animate: {
+                            enter: 'animated fadeIn',
+                            exit: 'animated fadeOutRight'
+                        },
+                    });
                 }
                 isSubmitting = false;
             }).catch(function(){
