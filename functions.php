efc126615dfabe0376b5318015cae3fb332708fd
Ronan <36049776+Runonstof@users.noreply.github.com>
2019-11-03 22:54:37 +0100

    require_files() fix

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 
 function require_files($directory, &$imported=[]) {
-
+    
     foreach(glob($directory.'*.*') as $file) {
         require $file;
     }
 
-    foreach(glob($directory.'*') as $file) {
+    foreach(glob($directory.'*.*') as $file) {
         require_files($file.'/');
     }
 }
