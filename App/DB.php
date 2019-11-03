7c1145c8ba71dfe528a056ffa46533db17aaee8f
Ronan <36049776+Runonstof@users.noreply.github.com>
2019-11-03 22:53:34 +0100

    Migrated from MySQLi to PDO for more database support

--- App/DB.php
+++ App/DB.php
@@ -2,52 +2,88 @@
 
 namespace App;
 
+use PDO;
+use stdClass;
+use PDOException;
+use PDOStatement;
+use Illuminate\Support\Collection;
+
 class DB {
 
     public static $db = null;
 
+    /**
+     * Initialized the connection to the database, if it fails and no callback is provided, an exception is thrown.
+     *
+     * @param Callable $callback
+     * @return void
+     */
     public static function init($callback=null) {
 
         if(self::$db == null) {
-            self::$db = @mysqli_connect(
-                $_ENV['DB_HOST'],
-                $_ENV['DB_USER'],
-                $_ENV['DB_PASS'],
-                $_ENV['DB_NAME']
-            );
-
-            if(\mysqli_connect_errno()) {
-                if(is_null($callback)) {
-                    if($_ENV['DEBUG_MODE']) {
-                        die('MYSQL ERROR: '.\mysqli_connect_error());
+            try {
+                self::$db = new PDO($_ENV['DB_TYPE'].":host=".$_ENV['DB_HOST'].";port=".$_ENV['DB_PORT'].";dbname=".$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+                // set the PDO error mode to exception
+                self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            } catch(PDOException $e) {
+                if(!is_callable($callback)) {
+                    if($_ENV['DEBUG_MODE']??false) {
+                        die('PDO ERROR: '.$e->getMessage());
                     } else {
-                        die('MYSQL ERROR!');
+                        die('PDO ERROR');
                     }
-                } elseif(is_callable($callback)) {
-                    $callback(mysqli_connect_errno(), mysqli_connect_error());
+                } else {
+                    $callback($e);
                 }
             }
         }
     
     }
 
-    
-
-    public static function errno() {
-        return \mysqli_connect_errno();
+    /**
+     * Closed the connection to the database
+     *
+     * @return void
+     */
+    public static function close() {
+        if(!is_null(self::$db)) {
+            self::$db = null;
+        }
     }
-
+    
+    /**
+     * List all databases
+     *
+     * @return Collection
+     */
     public static function databases() {
-        return self::select("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMA");
+        return self::select("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA");
     }
 
-    public static function tables($dbname) {
-        return self::select("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='{dbname}'", compact('dbname'));
+    /**
+     * Gets all tables of a specific database
+     *
+     * @param string $dbname
+     * @return Collection
+     */
+    public static function tables($dbname=null) {
+        
+        return self::select("SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema=:dbname;",[
+            'dbname' => is_null($dbname) ? $_ENV['DB_NAME'] : $dbname
+        ]);
     }
     
+    /**
+     * Check if a database exists
+     *
+     * @param string $dbname
+     * @return bool
+     */
     public static function dbexists($dbname) {
         return in_array($dbname, self::databases()->toArray());
     }
+    
     
     public static function exists() {
         $exists = true;
@@ -58,32 +94,62 @@ class DB {
         return $exists;
     }
 
-
+    /**
+     * Query the database, returns an already executed prepared statement
+     *
+     * @param string $sql
+     * @param array $values
+     * @return PDOStatement
+     */
     public static function query($sql, $values=[]) {
         self::init();
 
-        foreach($values as $key=>$value) { //Escape all arguments in the SQL
-            $sql = str_replace(':'.$key, \mysqli_real_escape_string($value, self::$db), $sql);
+        $statement = self::$db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        
+        foreach($values as $key=>$value) {
+            $statement->bindParam(':'.$key,$value);
         }
 
-        $query = self::$db->query($sql);
-
-        return $query;
-    }
-
-    public static function select($sql, $values=[],$assoc=true) {
-        $query_result = self::query($sql, $values);
-
-        $results = [];
-        if($query_result) {
-            if($query_result->num_rows > 0) {
-                //Loop through results
-                while($row = $query_result->{'fetch_'.($assoc ? 'assoc' : 'array')}()) {
-                    $results[] = $row; //Push $row in $results array
-                }
+        try {
+            $statement->execute();
+        } catch (PDOException $e) {
+            if($_ENV['DEBUG_MODE']) {
+                die('PDO ERROR: '.$e->getMessage()."<br><br>".print_r($statement,true));
+            } else {
+                die('PDO ERROR');
             }
         }
 
-        return collect($results);
+        return $statement;
+    }
+
+    /**
+     * Queries the database and fetches the data
+     *
+     * @param string $sql
+     * @param array $values
+     * @param boolean $assoc
+     * @return Collection
+     */
+    public static function select($sql, $values=[],$assoc=true) {
+        $statement = self::query($sql, $values);
+        
+        $queryResults = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if(!$assoc) {
+            $objs = [];
+            
+            foreach($queryResults as $row) {
+                $obj = new stdClass;
+                foreach($row as $key=>$value) {
+                    $obj->{$key} = $value;
+                }
+                $objs[] = $obj;
+            }
+
+            return collect($objs);
+        }
+
+        return collect($queryResults);
     }
 }
